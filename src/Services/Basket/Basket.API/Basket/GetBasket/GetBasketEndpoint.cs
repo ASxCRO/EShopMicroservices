42c5cbfd49a79cb091fadbccbf684c9b8ca19351
ASxCRO <antonio.suups@gmail.com>
2024-07-14 17:28:11 +0200

    basket handlers and endpoints

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -1,6 +1,26 @@
-﻿namespace Basket.API.Basket.GetBasket
+﻿
+
+
+namespace Basket.API.Basket.GetBasket
 {
-    public class GetBasketEndpoint
+    //public record GetBasketRequest(string UserName);
+    public record GetBasketRequest(string UserName);
+    public record GetBasketResponse(ShoppingCart Cart);
+    public class GetBasketEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("basket/{userName}", async (string userName, ISender sender) =>
+            {
+                var result = await sender.Send(new GetBasketQuery(userName));
+                var response = result.Adapt<GetBasketResponse>();
+                return Results.Ok(result);
+            })
+            .WithName("GetBasketByUserName")
+            .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("GetBasketByUserName")
+            .WithDescription("GetBasketByUserName");
+        }
     }
 }
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,20 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCarter();
+
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+    config.AddOpenBehavior(typeof(LoggingBehaviour<,>));
+});
+
 var app = builder.Build();
+
+app.MapCarter();
 
 app.Run();
